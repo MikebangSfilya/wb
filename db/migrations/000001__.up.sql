4cafdef56ea766740948bd5ba41d42e2ada70b9e
Sfilya <Typegoot@gmail.com>
2026-02-09 01:57:54 +0300

    sql fix

--- db/migrations/000001__.up.sql
+++ db/migrations/000001__.up.sql
@@ -1,36 +1,36 @@
 CREATE TABLE IF NOT EXISTS orders (
-    order_uid VARCHAR(255) PRIMARY KEY,
-    track_number VARCHAR(255) NOT NULL,
-    entry VARCHAR(255) NOT NULL,
-    locale VARCHAR(255) NOT NULL,
-    internal_signature VARCHAR(255),
-    customer_id VARCHAR(255) NOT NULL,
-    delivery_service VARCHAR(255) NOT NULL,
-    shardkey VARCHAR(255) NOT NULL,
+    order_uid TEXT PRIMARY KEY,
+    track_number TEXT NOT NULL,
+    entry TEXT NOT NULL,
+    locale TEXT NOT NULL,
+    internal_signature TEXT,
+    customer_id TEXT NOT NULL,
+    delivery_service TEXT NOT NULL,
+    shardkey TEXT NOT NULL,
     sm_id INTEGER NOT NULL,
     date_created TIMESTAMPTZ NOT NULL,
-    oof_shard VARCHAR(255) NOT NULL
+    oof_shard TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS delivery (
-    order_uid VARCHAR(255) PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
-    name VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    zip VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    region VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    order_uid TEXT PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    zip TEXT NOT NULL,
+    city TEXT NOT NULL,
+    address TEXT NOT NULL,
+    region TEXT NOT NULL,
+    email TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS payment (
-    transaction VARCHAR(255) PRIMARY KEY,
-    order_uid VARCHAR(255) NOT NULL REFERENCES orders(order_uid) ON DELETE CASCADE,
-    request_id VARCHAR(255),
-    currency VARCHAR(255) NOT NULL,
-    provider VARCHAR(255) NOT NULL,
+    transaction TEXT PRIMARY KEY,
+    order_uid TEXT NOT NULL REFERENCES orders(order_uid) ON DELETE CASCADE,
+    request_id TEXT,
+    currency TEXT NOT NULL,
+    provider TEXT NOT NULL,
     amount INTEGER NOT NULL CHECK (amount >= 0),
     payment_dt BIGINT NOT NULL,
-    bank VARCHAR(255) NOT NULL,
+    bank TEXT NOT NULL,
     delivery_cost INTEGER NOT NULL CHECK (delivery_cost >= 0),
     goods_total INTEGER NOT NULL CHECK (goods_total >= 0),
     custom_fee INTEGER NOT NULL CHECK (custom_fee >= 0)
@@ -38,16 +38,16 @@ CREATE TABLE IF NOT EXISTS payment (
 
 CREATE TABLE IF NOT EXISTS items (
     id SERIAL PRIMARY KEY,
-    order_uid VARCHAR(255) NOT NULL REFERENCES orders(order_uid) ON DELETE CASCADE,
+    order_uid TEXT NOT NULL REFERENCES orders(order_uid) ON DELETE CASCADE,
     chrt_id INTEGER NOT NULL,
-    track_number VARCHAR(255) NOT NULL,
+    track_number TEXT NOT NULL,
     price INTEGER NOT NULL CHECK (price >= 0),
-    rid VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    rid TEXT NOT NULL,
+    name TEXT NOT NULL,
     sale INTEGER NOT NULL CHECK (sale >= 0),
-    size VARCHAR(255) NOT NULL,
+    size TEXT NOT NULL,
     total_price INTEGER NOT NULL CHECK (total_price >= 0),
     nm_id INTEGER NOT NULL,
-    brand VARCHAR(255) NOT NULL,
+    brand TEXT NOT NULL,
     status INTEGER NOT NULL
 );
