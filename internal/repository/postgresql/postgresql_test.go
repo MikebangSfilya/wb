652f8fee0740cc098a4c60cc1f32198608730c05
Sfilya <Typegoot@gmail.com>
2026-02-17 02:59:45 +0300

    postre_test

---
+++ internal/repository/postgresql/postgresql_test.go
@@ -0,0 +1,195 @@
+package postgresql
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/MikebangSfilya/wb/internal/model"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+const initSQL = `
+CREATE TABLE IF NOT EXISTS orders (
+    order_uid TEXT PRIMARY KEY,
+    track_number TEXT NOT NULL,
+    entry TEXT NOT NULL,
+    locale TEXT NOT NULL,
+    internal_signature TEXT,
+    customer_id TEXT NOT NULL,
+    delivery_service TEXT NOT NULL,
+    shardkey TEXT NOT NULL,
+    sm_id INTEGER NOT NULL,
+    date_created TIMESTAMPTZ NOT NULL,
+    oof_shard TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS delivery (
+    order_uid TEXT PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    zip TEXT NOT NULL,
+    city TEXT NOT NULL,
+    address TEXT NOT NULL,
+    region TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS payment (
+    transaction TEXT PRIMARY KEY,
+    order_uid TEXT NOT NULL REFERENCES orders(order_uid) ON DELETE CASCADE,
+    request_id TEXT,
+    currency TEXT NOT NULL,
+    provider TEXT NOT NULL,
+    amount INTEGER NOT NULL CHECK (amount >= 0),
+    payment_dt BIGINT NOT NULL,
+    bank TEXT NOT NULL,
+    delivery_cost INTEGER NOT NULL CHECK (delivery_cost >= 0),
+    goods_total INTEGER NOT NULL CHECK (goods_total >= 0),
+    custom_fee INTEGER NOT NULL CHECK (custom_fee >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    order_uid TEXT NOT NULL REFERENCES orders(order_uid) ON DELETE CASCADE,
+    chrt_id INTEGER NOT NULL,
+    track_number TEXT NOT NULL,
+    price INTEGER NOT NULL CHECK (price >= 0),
+    rid TEXT NOT NULL,
+    name TEXT NOT NULL,
+    sale INTEGER NOT NULL CHECK (sale >= 0),
+    size TEXT NOT NULL,
+    total_price INTEGER NOT NULL CHECK (total_price >= 0),
+    nm_id INTEGER NOT NULL,
+    brand TEXT NOT NULL,
+    status INTEGER NOT NULL
+);
+`
+
+func TestPostgresRepository(t *testing.T) {
+	ctx := context.Background()
+
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:18-alpine",
+		postgres.WithDatabase("testdb"),
+		postgres.WithUsername("user"),
+		postgres.WithPassword("password"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+	require.NoError(t, err)
+
+	defer func() {
+		if err := pgContainer.Terminate(ctx); err != nil {
+			t.Logf("failed to terminate container: %s", err)
+		}
+	}()
+
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	require.NoError(t, err)
+
+	pool, err := pgxpool.New(ctx, connStr)
+	require.NoError(t, err)
+	defer pool.Close()
+
+	_, err = pool.Exec(ctx, initSQL)
+	require.NoError(t, err, "failed to init tables")
+
+	repo := New(pool)
+
+	fixedTime := time.Date(2023, 11, 26, 12, 0, 0, 0, time.UTC).Truncate(time.Microsecond)
+	order := &model.Order{
+		OrderUID:          "b563feb7b2b84b6test",
+		TrackNumber:       "WBILMTESTTRACK",
+		Entry:             "WBIL",
+		Locale:            "en",
+		InternalSignature: "",
+		CustomerID:        "test",
+		DeliveryService:   "meest",
+		Shardkey:          "9",
+		SmID:              99,
+		DateCreated:       fixedTime,
+		OofShard:          "1",
+		Delivery: model.Delivery{
+			Name:    "Test Testov",
+			Phone:   "+9720000000",
+			Zip:     "2639809",
+			City:    "Kiryat Mozkin",
+			Address: "Ploshad Mira 15",
+			Region:  "Kraiot",
+			Email:   "test@gmail.com",
+		},
+		Payment: model.Payment{
+			Transaction:  "b563feb7b2b84b6test",
+			RequestID:    "",
+			Currency:     "USD",
+			Provider:     "wbpay",
+			Amount:       1817,
+			PaymentDt:    1637907727,
+			Bank:         "alpha",
+			DeliveryCost: 1500,
+			GoodsTotal:   317,
+			CustomFee:    0,
+		},
+		Items: []model.Item{
+			{
+				ChrtID:      9934930,
+				TrackNumber: "WBILMTESTTRACK",
+				Price:       453,
+				Rid:         "ab4219087a764ae0btest",
+				Name:        "Mascaras",
+				Sale:        30,
+				Size:        "0",
+				TotalPrice:  317,
+				NmID:        2389212,
+				Brand:       "Vivienne Sabo",
+				Status:      202,
+			},
+		},
+	}
+
+	err = repo.CreateOrder(ctx, order)
+	require.NoError(t, err, "failed to create initial order for tests")
+
+	testCases := []struct {
+		name      string
+		uid       string
+		wantErr   error
+		wantOrder *model.Order
+	}{
+		{
+			name:      "existing_order",
+			uid:       order.OrderUID,
+			wantErr:   nil,
+			wantOrder: order,
+		},
+		{
+			name:      "non-existent_order",
+			uid:       "missing-uid-999",
+			wantErr:   model.ErrNotFound,
+			wantOrder: nil,
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			tOrder, err := repo.GetOrder(ctx, tt.uid)
+			if tt.wantErr != nil {
+				assert.ErrorIs(t, err, tt.wantErr)
+				return
+			}
+			require.NoError(t, err)
+
+			if tOrder != nil {
+				tOrder.DateCreated = tOrder.DateCreated.In(time.UTC)
+			}
+			assert.Equal(t, tt.wantOrder, tOrder)
+		})
+	}
+}
